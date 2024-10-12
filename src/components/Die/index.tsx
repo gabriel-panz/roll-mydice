c05424a77a89a1220eeb2a03ca789f289c6458d4
panzenhagen <gabriel.panzenhagen@gmail.com>
2024-10-12 01:02:55 -0300

    feat: simple rolling animation

--- src/components/Die/index.tsx
+++ src/components/Die/index.tsx
@@ -1,18 +1,25 @@
 import './index.css'
 import Icon from '../Icon'
 import { DiceType } from '../../interfaces/diceItem'
+import React from 'react'
 
-export interface DieProps {
+export interface DieProps extends React.HTMLProps<Element> {
     rollResult: number
     operator: string
     icon: DiceType
 }
 
 export default function Die(props: DieProps) {
+    let delay = `animation-delay:${props.key!}s`
     return (
         <div className="die">
             <p>{props.operator}</p>
-            <Icon fill='var(--dark-color)' name={props.icon} width={60} height={60}>
+            <Icon
+                fill='var(--dark-color)'
+                name={props.icon}
+                width={60}
+                height={60}
+            >
                 <span className='rollResult'>
                     <p>{props.rollResult}</p>
                 </span>
